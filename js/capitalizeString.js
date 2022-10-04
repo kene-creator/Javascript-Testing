0aa53aeb97500ca1704caf1ce376f201df9b52eb
Kene <kayyungx@gmail.com>
2022-10-04 16:46:42 +0100

    Add capitalize test

--- js/capitalizeString.js
+++ js/capitalizeString.js
@@ -1,5 +1,5 @@
 const capitalize = (string) => {
-  return string.charAt(0).toUpperCase() + str.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 };
 
 module.exports = capitalize;
--- js/capitalizeString.test.js
+++ js/capitalizeString.test.js
@@ -1,1 +1,12 @@
 const capitalize = require("./capitalizeString");
+
+//Arrange
+const successMessage = "capitalize first letter of string";
+
+//Act
+const capitalizeFirst = capitalize("victoria");
+
+//Assert
+test(successMessage, () => {
+  expect(capitalizeFirst).not.toMatch("victoria");
+});
